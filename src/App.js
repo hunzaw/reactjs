feef96202743eacbc34d3c789b88d653b46ee7e0
zaynkorai <zaynulabdin313@gmail.com>
2021-01-02 06:37:39 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sample React Js App by Hunzaw 
+        </p>
+        <a
+          className="App-link"
+          href="https://hunzaw.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Deploy More
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
